dca01ae632bd79d2ab817c7c9dfe566cdcdceb93
rameem2003 <rameem2019@gmail.com>
2024-11-13 15:23:43 +0600

    api added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import logo from '../../assets/images.jfif';
+import { useEffect, useState } from "react";
+import logo from "../../assets/images.jfif";
 import { AiOutlineMenuFold } from "react-icons/ai";
 import { CgClose } from "react-icons/cg";
 import { NavLink } from "react-router-dom";
@@ -8,17 +8,33 @@ import { PiStudentFill } from "react-icons/pi";
 import { GiTeacher } from "react-icons/gi";
 import { IoNotifications } from "react-icons/io5";
 import { RxBarChart } from "react-icons/rx";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { student } from "../../redux/feature/StudentsSlice";
 
 export default function Navbar() {
+  const dispatch = useDispatch(); // dispatch instance
   const [open, setOpen] = useState(false);
+
+  /**
+   * Fetch all students data
+   */
+
+  const fetchStudents = async () => {
+    let res = await axios.get(`${import.meta.env.VITE_API_URL}students`);
+    dispatch(student(res.data));
+  };
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
 
   return (
     <div>
       <div className="flex items-center justify-between text-xl font-bold px-8 py-5 bg-gray-300 ">
         <h1>Welcome To NUBian!</h1>
         <h1 className="hidden md:flex">Dashboard</h1>
-        
-        
+
         <div
           className="text-2xl md:hidden cursor-pointer"
           onClick={() => setOpen(!open)}
@@ -28,30 +44,99 @@ export default function Navbar() {
           {open ? <CgClose /> : <AiOutlineMenuFold />}
         </div>
 
-      
-        <div className={`absolute right-0   bg-white text-gray-400 px-6 shadow-lg mt-2 duration-500 md:hidden ${open?'top-12':'-top-[600px]'}`}>
-          <div className='py-5 flex flex-col items-center'>
-            <img src={logo} alt="NUB Logo" className=" w-10 h-10 md:h-24 mb-4 " />
-            <hr className='w-full mb-5' />
-            
-            
-            <NavLink to="/user" className="flex items-center gap-2 border-2 p-2 rounded-2xl mb-4">
+        <div
+          className={`absolute right-0   bg-white text-gray-400 px-6 shadow-lg mt-2 duration-500 md:hidden ${
+            open ? "top-12" : "-top-[600px]"
+          }`}
+        >
+          <div className="py-5 flex flex-col items-center">
+            <img
+              src={logo}
+              alt="NUB Logo"
+              className=" w-10 h-10 md:h-24 mb-4 "
+            />
+            <hr className="w-full mb-5" />
+
+            <NavLink
+              to="/user"
+              className="flex items-center gap-2 border-2 p-2 rounded-2xl mb-4"
+            >
               <img
-                className='rounded-full w-8'
+                className="rounded-full w-8"
                 alt="Admin Avatar"
                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
               />
-              <span className='font-bold'>Admin</span>
+              <span className="font-bold">Admin</span>
             </NavLink>
 
             {/* Navigation Links */}
             <nav className="text-xl flex flex-col gap-5 w-full">
-              <NavLink to="/" className={({ isActive }) => `${isActive ? 'bg-pink-500 text-white' : 'text-gray-600'} flex gap-2 items-center justify-start px-2 py-1 rounded-lg`}><MdSpaceDashboard />Dashboard</NavLink>
-              <NavLink to="/students" className={({ isActive }) => `${isActive ? 'bg-pink-500 text-white' : 'text-gray-600'} flex gap-2 items-center justify-start px-2 py-1 rounded-lg`}><PiStudentFill />Students</NavLink>
-              <NavLink to="/teachers" className={({ isActive }) => `${isActive ? 'bg-pink-500 text-white' : 'text-gray-600'} flex gap-2 items-center justify-start px-2 py-1 rounded-lg`}><GiTeacher />Teachers</NavLink>
-              <NavLink to="/notice" className={({ isActive }) => `${isActive ? 'bg-pink-500 text-white' : 'text-gray-600'} flex gap-2 items-center justify-start px-2 py-1 rounded-lg`}><IoNotifications />Notice</NavLink>
-              <NavLink to="/result" className={({ isActive }) => `${isActive ? 'bg-pink-500 text-white' : 'text-gray-600'} flex gap-2 items-center justify-start px-2 py-1 rounded-lg`}><RxBarChart />Result</NavLink>
-              <NavLink to="/videos" className={({ isActive }) => `${isActive ? 'bg-pink-500 text-white' : 'text-gray-600'} flex gap-2 items-center justify-start px-2 py-1 rounded-lg`}><MdOutlineSlowMotionVideo />Videos</NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "bg-pink-500 text-white" : "text-gray-600"
+                  } flex gap-2 items-center justify-start px-2 py-1 rounded-lg`
+                }
+              >
+                <MdSpaceDashboard />
+                Dashboard
+              </NavLink>
+              <NavLink
+                to="/students"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "bg-pink-500 text-white" : "text-gray-600"
+                  } flex gap-2 items-center justify-start px-2 py-1 rounded-lg`
+                }
+              >
+                <PiStudentFill />
+                Students
+              </NavLink>
+              <NavLink
+                to="/teachers"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "bg-pink-500 text-white" : "text-gray-600"
+                  } flex gap-2 items-center justify-start px-2 py-1 rounded-lg`
+                }
+              >
+                <GiTeacher />
+                Teachers
+              </NavLink>
+              <NavLink
+                to="/notice"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "bg-pink-500 text-white" : "text-gray-600"
+                  } flex gap-2 items-center justify-start px-2 py-1 rounded-lg`
+                }
+              >
+                <IoNotifications />
+                Notice
+              </NavLink>
+              <NavLink
+                to="/result"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "bg-pink-500 text-white" : "text-gray-600"
+                  } flex gap-2 items-center justify-start px-2 py-1 rounded-lg`
+                }
+              >
+                <RxBarChart />
+                Result
+              </NavLink>
+              <NavLink
+                to="/videos"
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "bg-pink-500 text-white" : "text-gray-600"
+                  } flex gap-2 items-center justify-start px-2 py-1 rounded-lg`
+                }
+              >
+                <MdOutlineSlowMotionVideo />
+                Videos
+              </NavLink>
             </nav>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import MainLayout from './components/MainLayout/MainLayout';
-import Dashboard from './components/Dashboard/Dashboard';
-import Students from './components/Students/Students';
-import Teachers from './components/Teachers/Teachers';
-import Notice from './components/Notice/Notice';
-import Video from './components/Video/Video';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import Studentsdetails from './components/Studentsdetails/Studentsdetails';
-import TeachersDetails from './components/TeachersDetails/TeachersDetails';
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./components/MainLayout/MainLayout";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Students from "./components/Students/Students";
+import Teachers from "./components/Teachers/Teachers";
+import Notice from "./components/Notice/Notice";
+import Video from "./components/Video/Video";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Studentsdetails from "./components/Studentsdetails/Studentsdetails";
+import TeachersDetails from "./components/TeachersDetails/TeachersDetails";
+import { Provider } from "react-redux";
+import Store from "./redux/app/Store";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
     errorElement: <ErrorPage></ErrorPage>,
-    children:[
+    children: [
       {
         path: "/",
         element: <Dashboard></Dashboard>,
@@ -29,39 +27,41 @@ const router = createBrowserRouter([
       {
         path: "/students",
         element: <Students></Students>,
-        loader : ()=> fetch('/studentsData.json')
+        loader: () => fetch("/studentsData.json"),
       },
       {
         path: "/teachers",
         element: <Teachers></Teachers>,
-        loader : ()=> fetch('/teachersData.json')
+        loader: () => fetch("/teachersData.json"),
       },
       {
         path: "/notice",
         element: <Notice></Notice>,
-        loader : ()=> fetch('/notice.json')
+        loader: () => fetch("/notice.json"),
       },
       {
         path: "/videos",
-        element: <Video></Video>  ,
-        loader : ()=> fetch('/video.json')
+        element: <Video></Video>,
+        loader: () => fetch("/video.json"),
       },
       {
         path: "/stddetails/:id",
-        element: <Studentsdetails></Studentsdetails>  ,
-        loader : ()=> fetch('/studentsData.json')
+        element: <Studentsdetails></Studentsdetails>,
+        loader: () => fetch("/studentsData.json"),
       },
       {
         path: "/tchdetails/:id",
-        element: <TeachersDetails></TeachersDetails>  ,
-        loader : ()=> fetch('/teachersData.json')
+        element: <TeachersDetails></TeachersDetails>,
+        loader: () => fetch("/teachersData.json"),
       },
-    ]
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <Provider store={Store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
